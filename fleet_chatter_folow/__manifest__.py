5cd5eba2ec1a21c5876d36e616d533c6ec0e9c9f
brameodoo <brame.odoo@gmail.com>
2025-04-20 09:45:17 -0600

    Add files via upload

---
+++ fleet_chatter_folow/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Fleet chatter folow',
+    'version': '1.0',
+    'depends': ['fleet'],
+    'data': [
+        'views/fleet_vehicle_inherit_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
