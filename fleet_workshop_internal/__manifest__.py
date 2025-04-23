4a0c8e4e9737af1f89c1f4098a3f24f875bc629b
brameodoo <brame.odoo@gmail.com>
2025-04-23 15:15:03 -0600

    Update __manifest__.py

--- fleet_workshop_internal/__manifest__.py
+++ fleet_workshop_internal/__manifest__.py
@@ -12,9 +12,9 @@
     'license': 'LGPL-3',
     'depends': ['base', 'fleet', 'hr', 'stock', 'mail'],
     'data': [
-        #'security/ir.model.access.csv',
-        'views/fleet_workshop_request_views.xml',
-        #'views/fleet_workshop_order_views.xml',  # Agregamos las vistas de la orden
+        'security/ir.model.access.csv',
+        #'views/fleet_workshop_request_views.xml',
+        'views/fleet_workshop_order_views.xml',  # Agregamos las vistas de la orden
         #'views/fleet_vehicle_views.xml',
         #'data/fleet_workshop_request_sequence.xml',
         #'data/fleet_workshop_order_sequence.xml',
