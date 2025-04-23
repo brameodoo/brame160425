a910bc064b9cc4f72f46819f8b4eb3a52620ce6c
brameodoo <brame.odoo@gmail.com>
2025-04-23 02:33:14 -0600

    Update __init__.py

--- fleet_workshop_internal/models/__init__.py
+++ fleet_workshop_internal/models/__init__.py
@@ -2,3 +2,6 @@
 from . import fleet_workshop_request
 from . import fleet_workshop_district
 from . import fleet_workshop_tag
+from . import fleet_workshop_order
+from . import fleet_workshop_repair_image
+from . import fleet_workshop_repair_line
