32f4adbf3c12ca12963adc9e8a1c0fad0b2ff629
brameodoo <brame.odoo@gmail.com>
2025-04-23 11:57:39 -0600

    Update __manifest__.py

--- fleet_workshop_internal/__manifest__.py
+++ fleet_workshop_internal/__manifest__.py
@@ -15,6 +15,7 @@
         'security/ir.model.access.csv',
         'views/fleet_workshop_request_views.xml',
         'views/fleet_workshop_order_views.xml',  # Agregamos las vistas de la orden
+        'views/fleet_vehicle_views.xml',
         'data/fleet_workshop_request_sequence.xml',
         'data/fleet_workshop_order_sequence.xml',
     ],
