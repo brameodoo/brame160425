e7c974c75e1769d3c4ec630c7d06dfd52d800126
brameodoo <brame.odoo@gmail.com>
2025-05-05 15:34:58 -0600

    Create  hr_expense.py

---
+++ expen_ajust/models/hr_expense.py
@@ -0,0 +1,13 @@
+class HrExpense(models.Model):
+    _inherit = 'hr.expense'
+
+    unverified_amount = fields.Float(string='Monto No Comprobado', readonly=True)
+
+    def action_adjust_expense(self):
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'expense.adjustment.wizard',
+            'view_mode': 'form',
+            'target': 'new',
+            'context': {'default_expense_id': self.id},
+        }
